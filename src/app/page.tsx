f123716c86de70a3609f2e6000745105c93c0758
CapSap <cshotam@gmail.com>
2024-05-22 14:46:48 +1000

    quick and dirty

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,61 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
+import { newData } from "./utils/newData";
+// import {data} from "./utils/csvjson"
 
 export default function Home() {
+  const [userInput, setUserInput] = useState("");
+
+  const userArray = userInput.split(/\s/);
+
+  function handleInputUpdate(e) {
+    if (userArray.length > 2) {
+      setUserInput(e.target.value);
+    } else {
+      setUserInput(e.target.value);
+    }
+  }
+
+  const search = (arr, str) => {
+    return arr.find((obj) => Object.values(obj).includes(str));
+  };
+
+  function isGood(input: string): boolean {
+    const skuObj = search(newData, userArray[0]);
+
+    console.log("found", skuObj);
+
+    if (!skuObj) {
+      return false;
+    }
+
+    const secondSearch = search([skuObj], userArray[1]);
+
+    console.log("2nd search", secondSearch);
+
+    return skuObj && secondSearch ? true : false;
+  }
+  console.log(newData);
+
+  console.log("isgood func", isGood(userInput));
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div id="container">
+      <textarea
+        name="skus"
+        id="skus"
+        value={userInput}
+        onChange={(e) => {
+          handleInputUpdate(e);
+        }}
+      ></textarea>
+      <p id="message">{isGood(userInput) ? "Box is good" : "bad"}</p>
+      <button id="clearInputButton" onClick={() => setUserInput("")}>
+        Clear input
+      </button>
+    </div>
   );
 }
